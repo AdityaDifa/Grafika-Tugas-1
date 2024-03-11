32cdcf532f8420f8cb3fa513b22b95496ddd95ff
Aditya Difa Amanullah <adityadifa123@gmail.com>
2024-03-11 21:00:59 +0700

    update

---
+++ algoritma_DDA.py
@@ -0,0 +1,96 @@
+import math
+import matplotlib.pyplot as plt
+
+class Algoritma_DDA:
+    x_increment = 0.
+    y_increment = 0.
+    step = 0.
+    dx = 0.
+    dy = 0.
+
+    def __init__(self,x1, y1,x2,y2):
+        self.nilai_x1 = x1
+        self.nilai_y1 = y1
+        self.nilai_x2 = x2
+        self.nilai_y2 = y2
+        self.temp_x = x1
+        self.temp_y = y1
+
+        #nilai x dan y awal
+        self.x_array = [self.nilai_x1]
+        self.y_array = [self.nilai_y1]
+
+
+    def hitung(self):
+        print("sedang menghitung")
+        #langkah 1 : cari selisih x dan y
+        self.dx = self.nilai_x2 - self.nilai_x1
+        self.dy = self.nilai_y2 - self.nilai_y1
+        print(f"nilai dx = {self.dx} dan nilai dy = {self.dy}")
+
+        #langkah 2 : tentukan step
+        if self.dy > self.dx: #jika dy lebih besar dari dx maka step = dy
+            self.step = self.dy
+            print(f"step = dy yaitu {self.step}")
+        else:
+            self.step = self.dx #jika tidak maka step = dx
+            print(f"step = dx yaitu {self.step}")
+
+        #langkah 3 : mencari nilai increment
+        print("menghitung step")
+        self.x_increment = self.dx/self.step 
+        self.y_increment = self.dy/self.step
+        print(f"nilai increment x dan y adalah {self.x_increment} dan {self.y_increment}")
+        print(f"array awal \nx = {self.x_array}\ny = {self.y_array}")
+
+        #perulangan nilai x dan y
+        while self.temp_x != self.nilai_x2 or self.temp_y != self.nilai_y2:
+            # self.temp_x += self.x_increment
+            # self.temp_y += self.y_increment
+            # self.x_array.append(self.temp_x)
+            # self.y_array.append(self.temp_y)
+            if self.temp_x != self.nilai_x2:
+                self.temp_x += self.x_increment
+                self.x_array.append(self.temp_x)
+            # else:
+            #     self.x_array.append(self.temp_x)
+
+            if self.temp_y != self.nilai_y2:
+                self.temp_y += self.y_increment
+                self.y_array.append(self.temp_y)
+            # else:
+            #     self.y_array.append(self.temp_y)
+
+
+        #langkah terakhir : memperbaiki nilai yang koma/ membulatkan semuanya
+        for indeks in range(len(self.x_array)):
+            if self.x_array[indeks] >= 4.5:
+                self.x_array[indeks] = math.ceil(self.x_array[indeks])
+            else:
+                self.x_array[indeks] = math.floor(self.x_array[indeks])
+
+        for indeks in range(len(self.y_array)):
+            if self.y_array[indeks] >= 4.5:
+                self.y_array[indeks] = math.ceil(self.y_array[indeks])
+            else:
+                self.y_array[indeks] = math.floor(self.y_array[indeks])
+        print("hitung selesai")
+
+    def show(self):
+        print(self.x_array)
+        print(self.y_array)
+        print("menampilkan tabel")
+
+        # Menambahkan judul dan label sumbu
+        plt.title('Plot Scatter dari Array x dan y')
+        plt.xlabel('Nilai x')
+        plt.ylabel('Nilai y')
+
+        #menambahkan grid
+        plt.grid(True)
+
+        # bikin garis
+        plt.scatter(self.x_array, self.y_array, color='red', marker='o', s=300)
+
+        # Menampilkan plot
+        plt.show()
---
+++ main.py
@@ -0,0 +1,160 @@
+import tkinter as tk
+from tkinter import ttk
+from algoritma_DDA import Algoritma_DDA as DDA
+
+#cek apakah inputan nilai atau angka
+def cek_angka(nilai_x, nilai_y,nilai_x2,nilai_y2):
+    #cek apakah nilai x dan y sudah integer atau belum
+    #jika belum maka false jika sudah true
+    global nilai_x_cek
+    global nilai_y_cek
+    global nilai_x_cek2
+    global nilai_y_cek2
+    if nilai_x.isdigit():
+        nilai_x_cek = True
+    else:
+        nilai_x_cek = False
+
+    if nilai_y.isdigit():
+        nilai_y_cek = True
+    else:
+        nilai_y_cek = False
+
+    if nilai_x2.isdigit():
+        nilai_x_cek2 = True
+    else:
+        nilai_x_cek2 = False
+
+    if nilai_y2.isdigit():
+        nilai_y_cek2 = True
+    else:
+        nilai_y_cek2 = False
+
+        
+    #cek keduanya buat konfirmasi langkah selanjutnya
+    if nilai_x_cek and nilai_y_cek and nilai_x_cek2 and nilai_y_cek2:
+        return True
+    else:
+        return False
+
+def Kerjakan():
+    if cek_angka(nilai_x.get(), nilai_y.get(), nilai_x2.get(),nilai_y2.get()):
+        x_konfirmasi.config(text="")
+        y_konfirmasi.config(text="")
+        x_konfirmasi2.config(text="")
+        y_konfirmasi2.config(text="")
+        print("semua nilai adalah integer")
+
+        #kerjakan rumus
+        if pilihan_menu.get() == "Algoritma DDA":
+            print("menu algoritma DDA terpilih")
+            tugas = DDA(float(nilai_x.get()),float(nilai_y.get()),float(nilai_x2.get()),float(nilai_y2.get()))
+            tugas.hitung()
+            tugas.show()
+            del tugas
+
+    else:
+        if nilai_x_cek == True:
+            x_konfirmasi.config(text="")
+        else:
+            x_konfirmasi.config(text="Masukan angka yang benar")
+            
+        if nilai_y_cek == True:
+            y_konfirmasi.config(text="")
+        else:
+            y_konfirmasi.config(text="Masukan angka yang benar")
+        
+        if nilai_x_cek2 == True:
+            x_konfirmasi2.config(text="")
+        else:
+            x_konfirmasi2.config(text="Masukan angka yang benar")
+            
+        if nilai_y_cek2 == True:
+            y_konfirmasi2.config(text="")
+        else:
+            y_konfirmasi2.config(text="Masukan angka yang benar")
+        
+#trigger tombol enter
+def trigger_tombol(event=None):
+    Kerjakan()
+
+#membuat tampilan utama
+window = tk.Tk()
+window.configure(bg="white")
+window.geometry("400x400")
+window.title("TUGAS-1")
+
+#variable
+nilai_x = tk.StringVar()
+nilai_y = tk.StringVar()
+nilai_x2 = tk.StringVar()
+nilai_y2 = tk.StringVar()
+
+
+#variable
+nilai_x_cek = False
+nilai_y_cek = False
+nilai_x_cek2 = False
+nilai_y_cek2 = False
+
+#variable
+pilihan_menu = tk.StringVar()
+
+#widget
+#frame
+main_frame = ttk.Frame(window)
+main_frame.pack(padx=10,pady=10,fill="x",expand=True)
+
+#x1 label
+x_label = ttk.Label(main_frame,text="Nilai X1:")
+x_label.pack(padx=10,fill="x",expand=True)
+
+x_input = ttk.Entry(main_frame,textvariable=nilai_x)
+x_input.pack(padx=10,fill="x",expand=False)
+
+x_konfirmasi = ttk.Label(main_frame,text="")
+x_konfirmasi.pack(pady=5,fill="x",expand=True)
+
+#y1 label
+y_label = ttk.Label(main_frame,text="Nilai Y1:")
+y_label.pack(padx=10,fill="x",expand=True)
+
+y_input = ttk.Entry(main_frame,textvariable=nilai_y)
+y_input.pack(padx=10,fill="x",expand=True)
+
+y_konfirmasi = ttk.Label(main_frame,text="")
+y_konfirmasi.pack(pady=5,fill="x",expand=True)
+
+#x2 label
+x_label2 = ttk.Label(main_frame,text="Nilai X2:")
+x_label2.pack(padx=10,fill="x",expand=True)
+
+x_input2 = ttk.Entry(main_frame,textvariable=nilai_x2)
+x_input2.pack(padx=10,fill="x",expand=False)
+
+x_konfirmasi2 = ttk.Label(main_frame,text="")
+x_konfirmasi2.pack(pady=5,fill="x",expand=True)
+
+#y label
+y_label2 = ttk.Label(main_frame,text="Nilai Y2:")
+y_label2.pack(padx=10,fill="x",expand=True)
+
+y_input2 = ttk.Entry(main_frame,textvariable=nilai_y2)
+y_input2.pack(padx=10,fill="x",expand=True)
+
+y_konfirmasi2 = ttk.Label(main_frame,text="")
+y_konfirmasi2.pack(pady=5,fill="x",expand=True)
+
+#menu dropdown
+# Membuat menu dropdown
+pilihan_dropdown = ttk.Combobox(main_frame, textvariable=pilihan_menu,state="readonly")
+pilihan_dropdown['values'] = ('Algoritma DDA', 'B...', 'Circle Midpoint')
+pilihan_dropdown.pack(pady=2)
+
+#button
+tombol = ttk.Button(main_frame,text="jalankan",command=Kerjakan)
+tombol.pack(fill='x',expand=True,padx=10,pady=10)
+window.bind('<Return>', trigger_tombol)
+
+#run
+window.mainloop()
